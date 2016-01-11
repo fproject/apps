e1087ae1cc54d05043a107631a6b3a8fd6699a0e
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-01-11 15:12:15 +0700

    Add js file for limit UI

---
+++ pkdrive/js/pkdrive.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#navigation.menu").remove();
+    $("#header .icon-caret").remove();
+    $("#settings #expanddiv.menu").remove();
+    $("#settings #expand img").remove();
+    $("#header a#owncloud").attr('href', '#');
+});
