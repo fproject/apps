561d09059559df8702be44f16902a71c114c0794
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-01-26 15:19:48 +0700

    JS for PKF-147

--- pkdrive/js/pkdrive.js
+++ pkdrive/js/pkdrive.js
@@ -1,4 +1,3 @@
 $(document).ready(function(){
-    $("#navigation.menu, #header .icon-caret, #settings #expanddiv.menu, #settings #expand img").remove();
-    $("#header a#owncloud").attr('href', '#');
+    $("header").remove();
 });
